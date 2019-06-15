e4547a1c59b84c0d99d5e82291a646ca6c6fe78a
yanomarcos <yanomarcos@gmail.com>
2019-06-15 14:44:41 -0300

    Adicionar arquivos de projeto.

---
+++ AlbumFotos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AlbumFotos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
